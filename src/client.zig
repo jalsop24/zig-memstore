e27fcc1bed2d499bba9515e73f5543629bd20ae4
james <j.alsop99@hotmail.com>
2024-08-27 23:36:09 +0100

    Added port cli argument to client

--- src/client.zig
+++ src/client.zig
@@ -1,11 +1,39 @@
 const std = @import("std");
 
-pub fn main() !void {
-    const localhost = std.net.Address.initIp4(.{ 127, 0, 0, 1 }, 9876);
+fn getPortFromArgs(allocator: std.mem.Allocator) !u16 {
+    var args = try std.process.argsWithAllocator(allocator);
+    defer args.deinit();
+    // Skip first argument (path to program)
+    _ = args.skip();
+    const raw_port = args.next() orelse {
+        std.log.info("Expected port as a command line argument\n", .{});
+        return error.NoPort;
+    };
+    return try std.fmt.parseInt(u16, raw_port, 10);
+}
 
-    const stream = try std.net.tcpConnectToAddress(localhost);
+pub fn main() !void {
+    var gpa_alloc = std.heap.GeneralPurposeAllocator(.{}){};
+    defer std.debug.assert(gpa_alloc.deinit() == .ok);
+    const allocator = gpa_alloc.allocator();
+
+    const port = try getPortFromArgs(allocator);
+    const localhost = std.net.Address.initIp4(.{ 127, 0, 0, 1 }, port);
+
+    std.log.info("Connecting to {}", .{localhost});
+    const stream = std.net.tcpConnectToAddress(localhost) catch |err| {
+        switch (@TypeOf(err)) {
+            std.net.TcpConnectToAddressError => {
+                std.log.info("Failed to connect to {}", .{localhost});
+                return;
+            },
+            else => {
+                return err;
+            },
+        }
+    };
     defer stream.close();
-    std.log.info("Connecting to {}\n", .{localhost});
+    std.log.info("Connected!", .{});
 
     const data = "Hello zig!";
     var writer = stream.writer();
