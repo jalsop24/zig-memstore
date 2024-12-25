fd0b3f97afe51e8a7de7f8baa963bf4dbe703535
jalsop24 <53493046+jalsop24@users.noreply.github.com>
2024-12-25 21:50:15 +0000

    wip

--- src/event_loop.zig
+++ src/event_loop.zig
@@ -62,3 +62,57 @@ pub fn create_epoll_loop(server: *std.net.Server) !EpollEventLoop {
     try new_loop.register_server_event(server.stream.handle);
     return new_loop;
 }
+
+const KqueueEventLoop = struct {
+    kqueue_fd: i32,
+
+    pub fn init(self: *KqueueEventLoop) !void {
+        self.kqueue_fd = try std.posix.kqueue();
+    }
+
+    pub fn wait_for_events(self: *KqueueEventLoop) !usize {
+
+        
+        const ready_events = std.posix.epoll_wait(
+            self.epoll_fd,
+            &self.events,
+            TIMEOUT_MS,
+        );
+
+        return ready_events;
+    }
+
+    pub fn register_client_event(self: *const KqueueEventLoop, client_fd: std.posix.socket_t) !void {
+        var epoll_event = std.os.linux.epoll_event{
+            .events = POLL.IN,
+            .data = .{
+                .fd = client_fd,
+            },
+        };
+        try std.posix.epoll_ctl(
+            self.epoll_fd,
+            EPOLL.CTL_ADD,
+            client_fd,
+            &epoll_event,
+        );
+    }
+
+    pub fn register_server_event(self: *const KqueueEventLoop, server_fd: std.posix.socket_t) !void {
+        // Add server event to poll state
+
+        std.posix.kevent(self.kqueue_fd, changelist: []const Kevent, eventlist: []Kevent, TIMEOUT_MS)
+
+        var listen_event = std.posix.Kevent{
+            .events = POLL.IN,
+            .data = .{
+                .fd = server_fd,
+            },
+        };
+        try std.posix.epoll_ctl(
+            self.epoll_fd,
+            EPOLL.CTL_ADD,
+            server_fd,
+            &listen_event,
+        );
+    }
+};
