a1d144620f510ef0a655cc536960aa68fc2ed86a
jalsop24 <53493046+jalsop24@users.noreply.github.com>
2025-01-22 00:19:37 +0000

    Initial intrusive structure pattern

--- src/types.zig
+++ src/types.zig
@@ -19,3 +19,71 @@ pub const String = struct {
         allocator.free(self.content);
     }
 };
+
+pub const Integer = struct {
+    content: u64,
+    node: Node,
+};
+
+pub const Node = struct {
+    next: ?*Node = null,
+};
+
+pub fn list_size(root_node: *const Node) !usize {
+    var current_node: ?*Node = @constCast(root_node);
+    var len: usize = 0;
+
+    while (current_node) |node| {
+        current_node = node.next;
+        len += 1;
+        if (current_node == root_node) {
+            return error.CircularReference;
+        }
+    }
+
+    return len;
+}
+
+pub fn container(comptime V: type) type {
+    return struct {
+        pub fn of(
+            node: *const V,
+            comptime T: type,
+            comptime field_name: []const u8,
+        ) *T {
+            const offset = @offsetOf(T, field_name);
+            const raw_node_ptr: [*]u8 = @ptrCast(@constCast(node));
+            const ptr: *T = @alignCast(@ptrCast(raw_node_ptr - offset));
+            return ptr;
+        }
+    };
+}
+
+test "data from node" {
+    const int = Integer{
+        .content = 123,
+        .node = .{},
+    };
+
+    const ptr = container(Node).of(
+        &int.node,
+        Integer,
+        "node",
+    );
+
+    try std.testing.expectEqual(ptr, &int);
+    try std.testing.expectEqual(int.node.next, null);
+}
+
+test "linked list length" {
+    var a = Node{};
+    var b = Node{
+        .next = &a,
+    };
+
+    try std.testing.expectEqual(2, list_size(&b));
+
+    a.next = &b;
+
+    try std.testing.expectError(error.CircularReference, list_size(&b));
+}
