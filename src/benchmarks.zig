13f00420922d730db0305f70c12e585a8072a927
jalsop24 <53493046+jalsop24@users.noreply.github.com>
2025-02-22 14:57:31 +0000

    Request / response benchmark

--- src/benchmarks.zig
+++ src/benchmarks.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
 
+const testing = @import("testing.zig");
 const types = @import("types.zig");
 
 const HashMap = types.HashMap;
@@ -13,8 +14,8 @@ test "hashmap" {
     defer arena.deinit();
 
     const num_ops = 100_000;
-    const timestamp = std.time.nanoTimestamp;
-    const unit = "ns";
+    const timestamp = std.time.microTimestamp;
+    const unit = "us";
 
     const start_ts = timestamp();
     for (0..num_ops) |i| {
@@ -27,4 +28,38 @@ test "hashmap" {
     const end_ts = timestamp();
     const elapsed_time = @as(f64, @floatFromInt(end_ts - start_ts)) / num_ops;
     std.debug.print("Elapsed time per op: {d:.2}{s}\n", .{ elapsed_time, unit });
+}
+
+test "request_response_io" {
+    const allocator = std.testing.allocator;
+    var mapping = try types.Mapping.init(allocator);
+    defer mapping.deinit();
+
+    var arena = std.heap.ArenaAllocator.init(allocator);
+    defer arena.deinit();
+
+    var server = testing.TestServer{
+        .mapping = mapping,
+    };
+
+    const client = try testing.TestClient.init(arena.allocator());
+    defer client.deinit();
+    client.server = &server;
+
+    const num_ops = 100_000;
+    const timestamp = std.time.nanoTimestamp;
+    const unit = "ns";
+
+    const start_ts = timestamp();
+    for (0..num_ops) |_| {
+        _ = try client.sendRequest(
+            .{ .Get = .{
+                .key = .{ .content = "a_key" },
+            } },
+        );
+    }
+    const end_ts = timestamp();
+
+    const elapsed_time = @as(f64, @floatFromInt(end_ts - start_ts)) / num_ops;
+    std.debug.print("Elapsed time per request: {d:.2}{s}\n", .{ elapsed_time, unit });
 }
